b1aa4d2b25f949a62462414ac0556471787bb94b
yuanwai <lxdhzzj@gmail.com>
2024-01-14 17:57:19 +0800

    阿包帮忙改进了试用section的横排问题

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -201,18 +201,21 @@
 
 
 
-<Section name="ctawithimg" class="my-16 grid grid-cols-1 lg:grid-cols-2">
-    <div>
-        <img src="/images/try.png" class="w-full lg:w-2/4" alt="dashboard" />
-    </div>
-    <div>
-        <h2>百闻不如一“践”</h2>
-        <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">我们希望得到关于您的信息，以及您在业务上的需求. 只需填写很少的信息，之后我们会将试用账号发到您填写的邮箱.</p>
-        <a href="/" class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
-          我要试用
-          <ArrowRightSolid size="md" class="ml-2 -mr-1" />
-        </a>
-    </div>
+<Section name="ctawithimg" class="my-20 flex grid grid-cols-1 lg:grid-cols-2">
+  <div class="flex">
+    <div class="flex-1 mx-10"> 
+      <img src="/images/try.png" alt="dashboard" />
+  </div>
+  <div class="flex-1 ml-2 mr-16">
+      <h1>百闻不如一“践”</h1>
+      <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">我们希望得到关于您的信息，以及您在业务上的需求. 只需填写很少的信息，之后我们会将试用账号发到您填写的邮箱.</p>
+      <a href="/" class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+        我要试用
+        <ArrowRightSolid size="md" class="ml-2 -mr-1" />
+      </a>
+  </div>
+  </div>
+    
 </Section>
 
       <Section name="pricing" sectionClass="my-16">
